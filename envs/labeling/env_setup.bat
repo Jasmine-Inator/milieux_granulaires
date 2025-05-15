4dc9bb0f670fea5e8889b2344d84475326b80baf
Callezo <leosebastiencaze@gmail.com>
2025-05-15 16:37:13 +0200

    Change setup.bat in labeling/running/training

--- envs/labeling/env_setup.bat
+++ envs/labeling/env_setup.bat
@@ -1,6 +1,9 @@
 @echo
 winget install python3.9
-virtualenv --python C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python39\python.exe
+pip install virtualenv
+virtualenv --python C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python39\python.exe venv
 cd venv/Scripts
 call activate.bat
 pip install labelImg
+deactivate
+cmd /k
--- envs/running/env_setup.bat
+++ envs/running/env_setup.bat
@@ -1,6 +1,7 @@
 @echo
 winget install python3.11
-virtualenv --python C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\python.exe
+pip install virtualenv
+virtualenv --python C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python311\python.exe venv
 cd venv/Scripts
 call activate.bat
 cd../..
@@ -9,15 +10,15 @@ pip install "pyyaml==5.4.1" --no-build-isolation
 pip install -r requirements.txt
 pip install "protobuf<3.20"
 cd venv/Lib/site-packages/object_detection
-for /f "tokens=*" %%G in ('cd') do set dpath=%%G
+for /f "tokens=*" %%G1 in ('cd') do set dpath=%%G1
 cd../../../../../../Tensorflow/models/research/object_detection
-for /f "tokens=*" %%G in ('cd') do set opath=%%G
-xcopy %opath% %dpath% /h /i /c /k /e /r /y
+for /f "tokens=*" %%G2 in ('cd') do set opath=%%G2
+xcopy %opath% %dpath% /s /e
 cd../../official
-for /f "tokens=*" %%G in ('cd') do set opath1=%%G
+for /f "tokens=*" %%G3 in ('cd') do set opath1=%%G3
 cd %dpath%
 mkdir official
 cd official
-for /f "tokens=*" %%G in ('cd') do set dpath1=%%G
-xcopy %opath1% %dpath1% /h /i /c /k /e /r /y
+for /f "tokens=*" %%G4 in ('cd') do set dpath1=%%G4
+xcopy %opath1% %dpath1% /s /e
 cmd /k
